7c0a35621be3f753959ced98be195de8ca54e074
Kevin Heinicke <kevin.heinicke@tu-dortmund.de>
2020-06-18 10:37:02 +0200

    Add missing files, update playground

--- LinkDef.h
+++ LinkDef.h
@@ -2,4 +2,7 @@
 #pragma link C++ class RooCubicSplineFun;
 #pragma link C++ class RooCubicSplineKnot;
 #pragma link C++ class RooAbsGaussModelEfficiency;
+#pragma link C++ class RooEffResModel;
+#pragma link C++ class RooEffConvGenContext;
+#pragma link C++ class RooGaussEfficiencyModel;
 #endif
---
+++ RooAbsEffResModel.cxx
@@ -0,0 +1,5 @@
+#include <RooResolutionModel.h>
+#include "RooAbsEffResModel.h"
+
+RooAbsEffResModel::~RooAbsEffResModel()
+{ }
